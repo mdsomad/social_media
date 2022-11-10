db50689b6631a7e8aed0011dc6cae4e0e3759acc
Md Somad <mdsomad10@gmail.com>
2022-11-10 20:19:18 +0530

    video part 7

--- lib/view/login/login_screen.dart
+++ lib/view/login/login_screen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:tech_media/res/component/input_text_field.dart';
 import 'package:tech_media/res/component/round_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,17 +12,46 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final emailController = TextEditingController();
+  final emailFocusNode = FocusNode();
+  
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RoundButton(
-            loading: false,
-            title: "Login",onPress: (){},)
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            InputTextField(
+              myController: emailController,
+             focusNode: emailFocusNode,
+              onFitedSubmittedValue:(newValue) {
+                
+              }, 
+              onValidator: (value) {
+                return value.isEmpty?"Enter email":null;
+              },
+               keyBoradType: TextInputType.emailAddress,
+                hint: "Email", 
+                obsureText: false
+
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+          
+            RoundButton(
+              loading: false,
+              title: "Login",onPress: (){},)
+          ],
+        ),
       ),
     );
   }
